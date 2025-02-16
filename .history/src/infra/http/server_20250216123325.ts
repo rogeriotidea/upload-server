042c616aaa54b428471ca9770a492595bb711f69
Rogério  Coelho <rogeriocoelho@Rogerios-Mac-mini.local>
2025-02-16 12:41:15 -0300

    project setup

---
+++ .history/src/infra/http/server_20250216123325.ts
@@ -0,0 +1,15 @@
+import { fastify } from 'fastify';
+import { fastifyCors } from 'fastify-cors';
+
+const server = fastify();
+
+server.register(fastifyCors, {
+  origin: '*',
+});
+
+server.listen({
+    post: 3333,
+    host: '0.0.0.0'
+}).then(() => {
+    console.log('Server is running on port 3333');
+})
